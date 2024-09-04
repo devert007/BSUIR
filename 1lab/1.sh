19f45cab80eef7bc127a86961a4ac019d74d610d
devert007 <kislydevert007@gmail.com>
2024-09-04 10:32:45 +0300

    some fix

--- 1lab/1.sh
+++ 1lab/1.sh
@@ -1,11 +1,14 @@
+
 read -p "Введите строку: " user_input_string
 echo "Вы ввели: $user_input_string"
 read -p "Введите имя каталога: " dir_name
 echo "Вы ввели: $dir_name"
-cd "$dir_name"
+let way_to_directory =$(find / -type d  -name "$dir_name")
+echo "$way_to_directory"
 
-for sub_dir in */; do
-  if [ -d "$sub_dir" ] && [ -r "$sub_dir" ]; then
-    echo "Привет, $sub_dir"
-  fi
-done
+# for sub_dir in */; do
+#   if [ -d "$sub_dir" ] && [ -r "$sub_dir" ]
+#    then
+#     echo "Привет, $sub_dir"
+#   fi
+# done
