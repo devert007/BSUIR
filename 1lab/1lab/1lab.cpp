60a4aced584a04aa389500c1d9ccf2aeef079945
devert007 <kislydevert007@gmail.com>
2024-09-09 06:04:01 +0300

    fix

---
+++ 1lab/1lab/1lab.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+
+class LongInt {
+private:
+    std::string longNumber;
+public:
+    LongInt(std::string) {
+    }
+};
+
+
+
+
+int main()
+{
+    LongInt a("100000000");
+    std::cout << "Hello World!\n";
+}
