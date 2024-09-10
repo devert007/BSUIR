73db747168262db220ae228349f52048aa0d958c
devert007 <kislydevert007@gmail.com>
2024-09-10 23:50:04 +0300

    add fixes

--- 1lab/1lab/1lab.cpp
+++ 1lab/1lab/1lab.cpp
@@ -7,11 +7,16 @@ class LongInt {
 private:
     std::string longNumber;
 public:
-    LongInt(const std::string& str) {
+    LongInt(const std::string& str) : longNumber(str) {}
+    operator int() {
 
     }
-
+    friend LongInt operator+(LongInt& strNum1,LongInt& strNum2);
 };
+LongInt operator+(LongInt& strNum1,LongInt& strNum2)
+{   
+    return LongInt();
+}
 
 
 
@@ -22,6 +27,7 @@ int main()
     std::cout << "Введите число: ";
     std::string input_string_num = "";
     std::getline(std::cin, input_string_num);
-    LongInt a(input_string_num);
+    LongInt a { input_string_num };
 
 }
+
