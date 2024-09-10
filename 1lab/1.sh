b9f5077f4d20588e75f43a6a6a6d0a213cdbf3ce
devert007 <kislydevert007@gmail.com>
2024-09-10 11:40:28 +0300

    add fixes to 1lab

--- 1lab/1.sh
+++ 1lab/1.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 directory_name="$1"
 search_string="$2"
-echo "$directory"
 
-directory=$(find / -type d -name "$directory_name")
+
 
 findFiles() {
- grep -r -l "$search_string" | xargs stat --format="Name: %n Size: %B" | less
+ grep -r -l "$search_string" | xargs stat --format="Name: %n  Size: %B"
 }
 
+directory=$(find / -type d -name $directory_name 2>/dev/null)
 
 #check directory existence
 if [ -d "$directory" ]; then
   if [ -r "$directory" ]; then
     cd "$directory"
-    echo "Your files with $search_string\ " | findFiles "$directory";
+    echo  -e "Your files with $search_string" 
+    findFiles "$directory";
     else
     echo "Нет доступа к каталогу";
   fi
