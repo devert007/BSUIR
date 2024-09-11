ec6cb62d28ebfc7aab45f41707ef579b64f457c9
devert007 <kislydevert007@gmail.com>
2024-09-11 23:47:34 +0300

    fixes

--- 1lab/1lab/1lab.cpp
+++ 1lab/1lab/1lab.cpp
@@ -1,18 +1,21 @@
 ﻿
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-const int MAX_INT = 2147483648;
-const int MIN_INT = -2147483648;
+const int MAX_INT = 2147483647;
+const int MIN_INT = -2147483647;
 
 class LongInt {
 private:
     std::string longNumber;
 public:
     LongInt(const std::string& str) : longNumber(str) {}
+    int size() { return longNumber.size(); }
+    std::string getLong() { return longNumber; }
     operator int() const {
         const int numberInInteger = std::stoll(longNumber); //convert string to long long number 
-            if (numberInInteger<MIN_INT || numberInInteger>MAX_INT) {
+            if (numberInInteger <= MIN_INT || numberInInteger >= MAX_INT) {
                std:: cout << "Ваше число превышает размер типа данныых integer\n";
             }
             else {
@@ -20,12 +23,19 @@ public:
             }
     }
 
-    //friend LongInt operator+(LongInt& strNum1,LongInt& strNum2);
+    friend LongInt operator+(LongInt& strNum1,LongInt& strNum2);
 };
-/*LongInt operator+(LongInt& strNum1, LongInt& strNum2)
+LongInt operator+(LongInt& strLeft, LongInt& strRight)
 {   
-    return LongInt();
-}*/
+    std::string sumResult (std::max(LongInt(strLeft), LongInt(strRight)), '0');//initialization resultSum like '0000000...0' (max of strings)
+
+    for (int i = 0; i < sumResult.size(); ++i) {
+        int digitOfFirst = (i < strLeft.size()) ?
+            strLeft.getLong()[strLeft.size() - 1 - i] - '0' : 0;
+        std::cout << digitOfFirst;
+    }
+    return sumResult;
+}
 
 
 
