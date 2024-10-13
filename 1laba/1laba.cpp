13ab54ccfda3b09a543c93b8ca82a8f232494904
devert007 <kislydevert007@gmail.com>
2024-10-13 21:51:49 +0300

    fix folfer

--- 1laba/1laba.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "D:/LABY/BSUIR/1laba/Longint.h"
-#include "D:/LABY/BSUIR/1laba/Longint.cpp"
-#include <iostream>
-int main() {
-    Longint a("-123");
-    std::cout << a.getLong() << '\n';
-    std::cout << "Hello";
-    return 0;
-}
