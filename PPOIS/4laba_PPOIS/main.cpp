bc2a5e699dbc1455ca947d97403a35793926869c
devert007 <kislydevert007@gmail.com>
2024-11-11 07:10:01 +0300

    add cmake for 4 laba

---
+++ PPOIS/4laba_PPOIS/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main(){
+  vector<string>msg{"hello","world"};
+  for(const string &word:msg){
+    cout<<word<<" ";
+  }
+}
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/LABY/BSUIR/PPOIS/4laba_PPOIS
+# Build directory: D:/LABY/BSUIR/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
